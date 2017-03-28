58d977fa00e1d4fc0c861420fd79aac5368b9f34
envynoiz <jorge.palomo@neurona.tech>
2017-03-28 10:15:34 -0600

    Initial commit.
    First countimer version!
    v1.0.0

---
+++ readme.md
@@ -0,0 +1,256 @@
+## Countimer
+Simple **jQuery** plugin to start a basic count up timer on any HTML element.
+
+## Dependencies
+* moment.js >= 2.18.1
+* jQuery >= 2.1.4
+
+> Make sure you've loaded all dependencies before use the plugin.
+
+## Install
+### Using bower or npm
+```
+bower install --save countimer
+npm install --save countimer
+```
+
+### Manual
+1. Install [jQuery](https://code.jquery.com/) or include `<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>`
+2. Install [moment.js](https://momentjs.com/) or include `<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>`
+3. Download the library and include it into your HTML code `<script type="text/javascript" src="dist/ez.countimer.min.js"></script>`
+
+## Usage
+Add the following block and attach the countimer to desire element:  
+```
+<script type="text/javascript">
+    $( document ).ready(function() {
+        $('.timer').countimer();
+    });
+</script>
+```
+```
+<span class="timer"></span> <br/>
+<input class="timer" type="text" value="" name="timer" />
+```
+The timer will start with the default options.
+```
+00:00:00
+```
+You can attach it either on any element who has the **value** attribute or those that work with **text** like a `span`, `div`, `p` and others.
+
+### Options
+- displayMode: One of the following display options:
+    * On seconds : 0 
+    * On minutes: 1
+    * On hours: 2
+    * Full timer: `3` (**default**)
+* enableEvents: To enable/disable the timer events (**default** `false`)
+* autoStart: Auto start the timer when rendered (**default** `true`)
+* useHours: Show/Hide the hours (**default** `true`)
+* minuteIndicator: A simple string located next to the seconds (**default** `''`)
+* secondIndicator: A simple string located next to the minutes (**default** `''`)
+* separator: Separator between each time block (**default** `':'`)
+* leadingZeros: Number of leading zeros in each time block (**default** `2`)
+* initHours: Init number of hours (**default** `0`)
+* initMinutes: Init number of minutes (**default** `0`)
+* initSeconds: Init number of seconds (**default** `0`)
+
+### Display modes
+Render the timer as:
+* Hours
+```
+$('.timer').countimer({
+    displayMode: 2,
+    initHours: 4,
+    leadingZeros: 3
+});
+```
+```
+004
+```
+* Minutes
+```
+$('.timer').countimer({
+    displayMode: 1,
+    initMinutes: 64,
+    leadingZeros: 3,
+    minuteIndicator: "'"
+});
+```
+```
+064'
+```
+* Seconds
+```
+$('.timer').countimer({
+    displayMode: 0,
+    initSeconds: 3600,
+    secondIndicator: '"'
+});
+```
+```
+3600"
+```
+* Full
+```
+// With hours (default)
+$('.timer').countimer({
+    initHours: 1,
+    initMinutes: 20,
+    secondIndicator: '"'
+});
+```
+```
+01:20:00"
+```
+```
+// Without hours
+$('.timer').countimer({
+    useHours: false,
+    initHours: 1,
+    initMinutes: 20,
+    secondIndicator: '"'
+});
+```
+```
+// The timer add the init hours into minutes value
+80:00"
+```
+
+### Init values
+You can start the timer with the desired hours, minutes and seconds.  
+The countimer will start with the data provided.
+```
+$('.timer').countimer({
+    initHours: 4,
+    initMinutes: 50,
+    initSeconds: 10
+});
+```
+```
+04:50:10 
+```
+It's possible combine the different times on the countimer, it always will print the correct value depending of the selected display mode.
+```
+// Default display mode
+$('.timer').countimer({
+    initHours: 2,
+    initMinutes: 120,
+    initSeconds: 60
+});
+```
+```
+04:01:00
+```
+```
+// Timer on minutes
+$('.timer').countimer({
+    leadingZeros: 4,
+    displayMode: 1,
+    initHours: 2,
+    initMinutes: 120,
+    initSeconds: 60,
+});
+```
+```
+0241
+```
+
+## Methods
+The following methods are available to control the countimer instance:
+### Start
+It starts the countimer using the provided `init` values.  
+```
+$('.timer').countimer('start');
+```
+>If the option `autoStart` is true, the timer will start automatically after displayed.
+### Resume
+Resume the countimer at the last time when it was stopped.
+```
+$('.timer').countimer('resume');
+```
+### Stop
+Stop the countimer.
+```
+$('.timer').countimer('stop');
+```
+### Stopped
+Return a band indicating if the countimer has been stopped or not yet.
+```
+$('.timer').countimer('stopped');
+```
+### Get current time
+Return the current time at the moment when the method was called.
+```
+$('.timer').countimer('current');
+```
+When you get the current time, the returned object will contain the formatted and unformatted displayed value, as well the original time representation.
+```
+// If the timer was displayed with the default "displayMode" and it has as value: 02:45:08
+{
+    displayedMode: {
+        formatted: '02:45:08',
+        unformatted: {
+            hours: 2,
+            minutes: 45,
+            seconds: 8
+        }
+    },
+    original: {
+        hours: 2,
+        minutes: 45,
+        seconds: 8
+    }
+}
+```
+```
+// If the timer was displayed with the default "displayMode", the option "useHours" is false and it has as value: 120:04
+{
+    displayedMode: {
+        formatted: '120:04',
+        unformatted: {
+            minutes: 120,
+            seconds: 4
+        }
+    },
+    original: {
+        hours: 2,
+        minutes: 0,
+        seconds: 4
+    }
+}
+```
+
+> The methods `start`, `resume` and `stop` are chainables, it means you can do something like that: `$('.timer').countimer('stop').current();`
+
+## Events
+For default the countimer events are disabled in order not to call them every second, minute or hour.  
+If you really want to use them, then set the option `enableEvents` to true.
+* On each hour
+```
+$('.timer').countimer({
+    enableEvents: true
+}).on('hour', function(evt, time){
+    console.log(time);
+});
+```
+* On each minute
+```
+$('.timer').countimer({
+    enableEvents: true
+}).on('minute', function(evt, time){
+    console.log(time);
+});
+```
+* On each second
+```
+$('.timer').countimer({
+    enableEvents: true
+}).on('second', function(evt, time){
+    console.log(time);
+});
+```
+
+Plugin available under MIT license (See LICENSE file)  
+
+Copyright © 2017 envynoiz
---
+++ src/countimer.js
@@ -0,0 +1,292 @@
+/*
+ *
+ *
+ *
+ * Copyright (c) 2017 envynoiz
+ * Licensed under the MIT license.
+ */
+(function ($) {
+  'use strict';
+
+  var prefix = 'plugin_';
+  var pluginName = 'countimer';
+
+  $.plugin = function(element, options) {
+    // Main instance
+    var plugin = this;
+
+    plugin.options = {};
+    plugin.duration = {};
+
+    // Private attributes
+    const displayMode = {
+      ON_SECONDS : 0,
+      ON_MINUTES : 1,
+      ON_HOURS : 2,
+      FULL: 3,
+      MAX_INDEX: 3
+    };
+
+    const eventNames = [
+      'second',
+      'minute',
+      'hour'
+    ];
+
+    var defaultSettings = {
+      displayMode : displayMode.FULL,
+      enableEvents: false,
+      autoStart : true,
+      useHours : true,
+      minuteIndicator: '',
+      secondIndicator: '',
+      separator : ':',
+      leadingZeros: 2,
+      initHours : 0,
+      initMinutes : 0,
+      initSeconds: 0
+    };
+
+    var elm;
+    var $elm;
+    var timer;
+    var isStopped;
+    var hasValueAttr;
+    var refreshMode;
+    var displayResults;
+
+    // Private functions
+    var leadingZeros = function(num, width) {
+      var z = '0';
+      var n = ''.concat(num);
+      // If the number has the same width only return it again.
+      return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    };
+
+    var format = function(time, withSeparator){
+      var pre = leadingZeros(time, plugin.options.leadingZeros);
+      return withSeparator? pre.concat(plugin.options.separator) : pre;
+    };
+
+    var fixModeValue = function(value, maxIndex){
+      var band = (value === '' || value > maxIndex);
+      return band? maxIndex : value;
+    };
+
+    var getDefaultDuration = function(){
+      return moment.duration({
+        seconds : plugin.options.initSeconds,
+        minutes : plugin.options.initMinutes,
+        hour : plugin.options.initHours
+      });
+    };
+
+    var getResponseTimer = function(){
+      return {
+        displayedMode: displayResults[plugin.options.displayMode](),
+        original: $.extend({}, timer)
+      };
+    };
+
+    var eventHandler = function(refreshConditions){
+      if(plugin.options.enableEvents){
+        // Find events to fire up
+        $.each(refreshConditions, function (index) {
+          // Trigger events
+          if(refreshConditions[index]){
+            $elm.trigger(eventNames[index], getResponseTimer());
+          }
+        });
+      }
+    };
+
+    var mainTimer = function (omitRefresh) {
+      // Global timer values
+      timer = {
+        hours: plugin.duration.hours(),
+        minutes: plugin.duration.minutes(),
+        seconds: plugin.duration.seconds()
+      };
+      var getObjExpression = function(nf, f){
+        return {
+          unformatted: nf,
+          formatted: f
+        };
+      };
+      // Possible expressions to print
+      var expressions = {
+        toSeconds : function(){
+          var withoutFormat = Math.trunc(plugin.duration.asSeconds());
+          var formatted = format(withoutFormat).concat(plugin.options.secondIndicator);
+          return getObjExpression({seconds: withoutFormat}, formatted);
+        },
+        toMinutes : function(){
+          var withoutFormat = Math.trunc(plugin.duration.asMinutes());
+          var formatted = format(withoutFormat).concat(plugin.options.minuteIndicator);
+          return getObjExpression({minutes: withoutFormat}, formatted);
+        },
+        toHours : function() {
+          var withoutFormat = Math.trunc(plugin.duration.asHours());
+          return getObjExpression({hours: withoutFormat}, format(withoutFormat));
+        },
+        full : function() {
+          // Normal display
+          var normalNf = $.extend({}, timer);
+          var normal = format(timer.hours, true).concat(format(timer.minutes, true)).concat(format(timer.seconds));
+          normal = normal.concat(plugin.options.secondIndicator);
+          // Minutes display
+          var minutesObj = expressions.toMinutes();
+          var minutesNf = {
+            minutes: minutesObj.unformatted.minutes,
+            seconds: timer.seconds
+          };
+          var minutes = format(minutesNf.minutes, true).concat(format(minutesNf.seconds));
+          minutes = minutes.concat(plugin.options.secondIndicator);
+          return (plugin.options.useHours)? getObjExpression(normalNf, normal) : getObjExpression(minutesNf, minutes);
+        }
+      };
+      // Array with refresh conditions
+      var refreshConditions = [
+        true, // each second
+        0 === timer.seconds, // each minute
+        0 === timer.minutes && 0 === timer.seconds // each hour
+      ];
+      // Array with each function expression
+      displayResults = [
+        expressions.toSeconds,
+        expressions.toMinutes,
+        expressions.toHours,
+        expressions.full
+      ];
+      // Set final value into element
+      if(omitRefresh || refreshConditions[refreshMode]){
+        // Call the method depending of displayMode
+        var finalValue = displayResults[plugin.options.displayMode]();
+        hasValueAttr? $elm.val(finalValue.formatted) : $elm.text(finalValue.formatted);
+      }
+      // Find events to fire up
+      eventHandler(refreshConditions);
+      // Update moment duration
+      plugin.duration = moment.duration(plugin.duration.asSeconds() + 1, 'seconds');
+    };
+
+    var startTimer = function(){
+      // Only if the timer is already stopped
+      if(isStopped){
+        // First call
+        mainTimer(true);
+        // Init interval
+        plugin.intervalFunction = setInterval(mainTimer, 1000);
+        isStopped = false;
+        // In order to allow chaining calls
+        return plugin;
+      }
+    };
+
+    // Init function to initialize some awesome stuffs
+    var init = function() {
+      // Initialize public variables
+      plugin.options = $.extend({}, defaultSettings, options);
+      // Validate the max index
+      plugin.options.displayMode = fixModeValue(plugin.options.displayMode, displayMode.MAX_INDEX);
+      // Configure moment duration
+      plugin.duration = getDefaultDuration();
+      // Initialize private variables
+      elm = element;
+      $elm = $(elm);
+      isStopped = true;
+      hasValueAttr = undefined !== $elm.attr('value');
+      refreshMode = plugin.options.displayMode <= displayMode.ON_HOURS? plugin.options.displayMode : displayMode.ON_SECONDS;
+      // Start?
+      if(plugin.options.autoStart){
+        plugin.start();
+      }else{
+        mainTimer(true);
+      }
+    };
+
+    // Start count timer from initial values
+    plugin.start = function(){
+      plugin.duration = getDefaultDuration();
+      return startTimer();
+    };
+
+    // Resume the counter
+    plugin.resume = function(){
+      return startTimer();
+    };
+
+    // Stop the counter
+    plugin.stop = function(){
+      if(plugin.intervalFunction){
+        clearInterval(plugin.intervalFunction);
+        isStopped = true;
+      }
+      // In order to allow chaining calls
+      return plugin;
+    };
+
+    // Band to know if the counter has been stopped or not
+    plugin.stopped = function(){
+      return isStopped;
+    };
+
+    // Return the current time
+    plugin.current = function(){
+      return getResponseTimer();
+    };
+
+    // Fire up the plugin calling its "constructor" method
+    init();
+  };
+
+  // Init jQuery plugin
+  $.fn[ pluginName ] = function(methodOrOptions) {
+    var method = ('string' === typeof methodOrOptions) ? methodOrOptions : undefined;
+
+    if (method) {
+      var plugins = [];
+
+      // Each HTML elements and get the current plugin instance
+      this.each(function() {
+        plugins.push( $(this).data(prefix.concat(pluginName)) );
+      });
+
+      // Retrieve arguments from params
+      var args = (arguments.length > 1) ? Array.prototype.slice.call(arguments, 1) : undefined;
+      var results = [];
+
+      var applyMethod = function(index) {
+        var plugin = plugins[index];
+        // Is plugin already instanced ?
+        if (!plugin) {
+          throw new Error(pluginName.concat(' $.plugin is not instantiated yet'));
+        }
+        // Does the method exist?
+        if ('function' !== typeof plugin[method]) {
+          throw new Error('Method '.concat(method).concat(' is not defined on ').concat(pluginName).concat(' $.plugin'));
+        }
+        // Call function and preserve result
+        results.push(plugin[method].apply(plugin, args));
+      };
+
+      // Call the method in each instance
+      this.each(applyMethod);
+
+      // Return one or more results depending of the number of HTML elements
+      return (results.length > 1) ? results : results[0];
+    }
+
+    // It's not a method
+    var options = ('object' === typeof methodOrOptions) ? methodOrOptions : undefined;
+
+    return this.each( function() {
+      // If the instance doesn't exist, then initialize it
+      if ( !$(this).data(prefix.concat(pluginName)) ) {
+        $(this).data(prefix.concat(pluginName), new $.plugin(this, options));
+      }
+    });
+
+  };
+
+}(jQuery));
